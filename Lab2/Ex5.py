0417e4b874029595ffff8217fde023e55fd879c1
GeorgeLopez81 <lopezg@hawaii.edu>
2024-09-07 20:27:29 -1000

    recent updates

---
+++ Lab2/Ex5.py
@@ -0,0 +1,7 @@
+#Ask the user to enter an arbitary sentence. Calculate the length of the string and
+#return that value.
+
+sentence = input("Enter a sentence: ")
+
+String_length = len(sentence)
+print("You entered: " , sentence, ". It has a length of: " ,String_length)
---
+++ Lab2/Ex7.1.py
@@ -0,0 +1,13 @@
+#Ask the user to input a temperature in degrees Fahrenheit.
+#Convert that temperature to Celsius and outpu it.
+
+#Function to convert degrees F to degrees C
+def FtoC(temperatureF)
+    degreesC = (float(degreesF) - 32) * (5/9)
+    return(degreesC)
+
+degreesF = input("Enter a temperature in Fahrenheit: ")
+
+
+
+print("This converts to " , FtoC(degreesF, "Celsius")
