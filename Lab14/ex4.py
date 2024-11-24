9749a9173aa3e16da1deac36d056d4a8c683ef66
GeorgeLopez81 <lopezg@hawaii.edu>
2024-11-23 19:52:54 -1000

    Lab 14 complete

--- Lab14/ex4.py
+++ Lab14/ex4.py
@@ -2,18 +2,18 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-trips_df=pd.read_json("../Trips from area 8.json")
+trips_df = pd.read_json("../Trips from area 8.json")
 
-trip_miles_gt_o = trips_df[['trip_miles', 'fare']].query('trip_miles > 0')#when analyzing the data ingnore the "0"
-fare_series = trip_miles_gt_o.to_fare
-trip_miles= trip_miles_gt_o.trip_miles
-#use trianagles to make the visualization better
+trip_miles_gt_0 = trips_df[['trip_miles', 'fare']].query('trip_miles > 0')
+fare_series = trip_miles_gt_0.fare 
+trip_miles = trip_miles_gt_0.trip_miles
+
 #plt.plot(fare_series, trip_miles, linestyle="none", marker=".")
 plt.plot(fare_series, trip_miles, marker="v", linestyle="none", color='c', alpha=0.2)
 #plt.scatter(fare_series, trip_miles)
-plt.title("Fares by Taxi Trip Miles")
+plt.title("Fares by Traxi Trip Miles")
 plt.ylabel("Distance in Miles")
 plt.xlabel("Total Fare in Dollars")
 
-plt.savefig("FaresXMiles.png", dpi=300)#creates a file
+plt.savefig("FaresXMiles.png", dpi=300)
 plt.show()
--- Lab14/ex5.py
+++ Lab14/ex5.py
@@ -2,23 +2,21 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#Use different plot styles
+# Use different plot styles
 #plt.style.use('ggplot')
 #plt.style.use("fivethirtyeight")
 plt.style.use("dark_background")
 
-trips_df=pd.read_json("../Trips from area 8.json")
-trip_miles_gt_o = trips_df[['trip_miles', 'fare']].query('trip_miles > 2')
+trips_df = pd.read_json("../Trips from area 8.json")
+trip_miles_gt_0 = trips_df[['trip_miles', 'fare']].query('trip_miles > 2')
 
-fare_series = trip_miles_gt_o.to_fare
-trip_miles= trip_miles_gt_o.trip_miles
+fare_series = trip_miles_gt_0.fare 
+trip_miles = trip_miles_gt_0.trip_miles
 
-plt.plot(fare_series, trip_miles, marker="0", linestyle="none", alpha=0.3)
-plt.title("Fare by Taxi Trip Miles >2")
-plt.xlabel("Fare")
+plt.plot(fare_series, trip_miles, marker="o", linestyle="none", alpha=0.3)
+plt.title("Fare by Taxi Trip Miles > 2")
+plt.xlabel("Fare in $")
 plt.ylabel("Distance in Miles")
 
-plt.legend()
-
-plt.savefig("FaresXMiles.2.png", dpi=300)#creates a file
+plt.savefig("FaresXMiles.2.png", dpi=300)
 plt.show()
